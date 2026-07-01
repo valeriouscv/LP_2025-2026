e1a0d7403ac8997f509a093b2f0e385d90eaab20
Valério Santos <valerio.santos@us.edu.cv>
2026-07-01 01:47:47 -0100

    aula02

---
+++ aula02/Ex01.java
@@ -0,0 +1,14 @@
+package aula02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int numBin = 0b1101;
+        int numOct = 010;
+        int numHexa = 0xA;
+        char letra = (char) 97;
+        System.out.println(numBin);
+        System.out.println(numOct);
+        System.out.println(numHexa);
+        System.out.println(letra);
+    }
+}
---
+++ aula02/Ex02.java
@@ -0,0 +1,36 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        //int num;
+       // String texto;
+        Scanner ler = new Scanner(System.in);
+
+        //System.out.print("Digite um numero: ");
+        //num = ler.nextInt();
+        
+        //ler em branco
+        //ler.nextLine();   //vai ler o enter
+        
+       // System.out.print("Texto: ");
+       // texto = ler.nextLine();
+
+       // System.out.println("O numero lido: "+num);
+       // System.out.println("o texto lido: "+texto);
+
+    
+        //short numShort = ler.nextShort();
+        //long numLong = ler.nextLong();
+        //float numFloat = ler.nextFloat();
+        //double numDouble = ler.nextDouble();
+        
+        //não existe um ler char
+        //char letra = ler.nextChar();
+        
+        //para ler letra, temos que ler um texto e tirar a primeira letra (posição: 0)
+        //char letra = ler.nextLine().charAt(0);
+       
+    }
+}
